0edb9c6e6ecd06e716e6b591fb45a051abf7511b
Ronald Schouw <mail@ronalt.nl>
2021-04-20 21:32:20 +0200

    first commit

---
+++ inventory.tf
@@ -0,0 +1,13 @@
+data "template_file" "inventory" {
+  #template = "${file("${path.module}/hosts.kubernetes")}"
+  template = file("${path.module}/hosts.kubernetes")
+  vars = {
+    master_hostname = digitalocean_droplet.master.ipv4_address
+    worker1_hostname = digitalocean_droplet.worker1.ipv4_address
+    worker2_hostname = digitalocean_droplet.worker2.ipv4_address
+  }
+}
+resource "local_file" "inventory" {
+  content  = data.template_file.inventory.rendered
+  filename = "${path.module}/hosts"
+}
---
+++ variable.tf
@@ -0,0 +1,7 @@
+variable "do_token" {
+  description = "DigitalOcean Api Token"
+}
+variable "ssh_key_fingerprint" {
+  description = "Fingerprint of the public ssh key stored on DigitalOcean"
+}
+variable "pvt_key" {}
