31c49f3495e7a9f0158b714ec84c434552af3961
Ronald Schouw <mail@ronalt.nl>
2021-06-28 20:00:08 +0200

    ckad

--- inventory.tf
+++ inventory.tf
@@ -4,7 +4,6 @@ data "template_file" "inventory" {
   vars = {
     master_hostname  = digitalocean_droplet.master.ipv4_address
     worker1_hostname = digitalocean_droplet.worker1.ipv4_address
-    worker2_hostname = digitalocean_droplet.worker2.ipv4_address
   }
 }
 resource "local_file" "inventory" {
